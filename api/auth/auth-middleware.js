d8f3e7fd92939b8e36097cabc234b43149836c8a
tommybrits951 <tommybrits74@gmail.com>
2023-03-17 01:22:23 -0700

    finished with a little guidance pun intended

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,11 +1,30 @@
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../../config/index')
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization
+  if (token) {
+     jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({status: 401, message: `token bad: ${err.message}`})
+      } else {
+        req.decodedJwt = decoded
+        console.log(req.decodedJwt)
+        next()
+      }
+     })
+  } else {
+    next({ status: 401, message: 'wat? no token?'})
+  }
 }
 
 // AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
+const checkRole = role => (req, res, next) => {
+  if (req.decodedJwt && req.decodedJwt.role === role) {
+    next()
+  } else {
+    next({status: 403, message: `You have no power here`})
+  }
 }
 
 module.exports = {
